c754672b9c34da4d8ec40a9d6bdc5b091ee22e7f
Eduardo Estrada <me@e2rd0.com>
2022-06-15 12:30:58 +0200

    Movimiento pacman y puntos

--- eduardo.estrada/pacman.java
+++ eduardo.estrada/pacman.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class pacman {
 
     private static int puntos = 0;
+    private static int movimientos = 0;
 
     public static void main(String[] args) {
         int[][] elMapa = {
@@ -34,8 +37,10 @@ public class pacman {
             {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1},				
             {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}		
         };
-        int[] personaje = {0,13};
-		imprimeMundo(elMapa, personaje);
+        int[] personaje = {22,13};
+		do {
+			imprimeMundo(elMapa, personaje);
+		} while (procesaMovimiento(elMapa, personaje));
     }
 
     private static void limpiaPantalla() {
@@ -82,9 +87,96 @@ public class pacman {
 
     private static void imprimeStatus(int[] elPersonaje) {
 
-		System.out.println("El personaje está en X:[" + elPersonaje[0] + "] Y:[" + elPersonaje[1] + "]");
-		System.out.println("Tienes [ "+ puntos +" ] puntos");
+		System.out.println("El personaje está en X:[" + elPersonaje[0] + "] Y:[" + elPersonaje[1] + "] | Tienes [ "+ puntos +" ] puntos | Haz hecho [ "+ movimientos +" ] movimientos");
 
+    }
+
+
+    private static boolean procesaMovimiento(int[][] elMapa, int[] elPersonaje) {
+
+		Scanner entrada = new Scanner(System.in);
+		String inputUsuario;
+		char laDireccion=' ';
+
+		inputUsuario = entrada.nextLine();
+
+		if (inputUsuario.equals("f")) {return false;} else
+		if (inputUsuario.equals("w")) {laDireccion='N';} else
+		if (inputUsuario.equals("a")) {laDireccion='O';} else
+		if (inputUsuario.equals("s")) {laDireccion='S';} else
+		if (inputUsuario.equals("d")) {laDireccion='E';} 
+
+		mueve(elPersonaje, elMapa, laDireccion);
+		return true;
+	}
+
+	private static void mueve(int[] unPersonaje, int[][] unMapa, char unaDireccion ){
+		int elPersonajeX, elPersonajeY;
+		elPersonajeX = unPersonaje[0];
+		elPersonajeY = unPersonaje[1];
+
+        boolean seMueve = false;
+
+		if (unaDireccion=='O') {
+			if (elPersonajeX == 0) { 
+                elPersonajeX = unMapa[0].length - 1; 
+                seMueve= true;
+            } 
+			else if  (unMapa[elPersonajeY][elPersonajeX - 1] % 2 == 0) {            
+                elPersonajeX = elPersonajeX - 1; 
+                seMueve= true;
+            }
+		} else if (unaDireccion=='N') {
+			if (elPersonajeY == 0) {
+                elPersonajeY = unMapa.length - 1;
+                seMueve= true;
+            } 
+			else if  (unMapa[elPersonajeY - 1][elPersonajeX] % 2 == 0) {        
+                elPersonajeY = elPersonajeY - 1;
+                seMueve= true;
+            }
+		} else if (unaDireccion=='E') {
+			if (elPersonajeX == unMapa[0].length - 1) {
+                 elPersonajeX = 0;
+                 seMueve= true;
+            } 
+			else if  (unMapa[elPersonajeY][elPersonajeX + 1] % 2 == 0) {
+                 elPersonajeX = elPersonajeX + 1; 
+                 seMueve= true;
+                }
+		} else if (unaDireccion=='S') {
+			if (elPersonajeY == unMapa.length - 1) {
+                 elPersonajeY = 0;
+                 seMueve= true;
+            } 
+			else if  (unMapa[elPersonajeY + 1][elPersonajeX] % 2 == 0) { 
+                elPersonajeY = elPersonajeY + 1; 
+                seMueve= true;
+            }
+		} 
+
+		unPersonaje[0] = elPersonajeX;
+		unPersonaje[1] = elPersonajeY;
+        
+        registraPuntos(unPersonaje, unMapa);
+        registraMovimiento(seMueve);
+	}
+
+    private static void registraPuntos(int[] personaje, int[][] unMapa){
+        if(unMapa[personaje[1]][personaje[0]] == 2){
+            unMapa[personaje[1]][personaje[0]] = 0;
+            puntos++;
+        }
+        else if(unMapa[personaje[1]][personaje[0]] == 4){
+            unMapa[personaje[1]][personaje[0]] = 0 ;
+            puntos+=5;
+        }
+    }
+
+    private static void registraMovimiento(boolean seMueve){
+        if(seMueve){
+            movimientos++;
+        }
     }
 
     private static String INICIO = "\033[";
